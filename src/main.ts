4a693bc501df05a6309e0c9a6fbb76cb21cb0cb1
Ruhul Amin <ruhul.amin@apsissolutions.com>
2022-11-27 23:48:25 +0600

    swagger and prisma setup done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DBService } from './db.service';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const dbService: DBService = app.get(DBService);
+  dbService.enableShutdownHooks(app);
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API Description')
+    .setVersion('0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
