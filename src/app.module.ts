d4af42ca427a3df9c4cf9aa43eab2096968efb61
Ruhulcse <ruhul.cse7862@gmail.com>
2022-12-05 23:41:45 +0600

    authGuard and jwt passport implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaService } from 'prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { prismaModule } from 'prisma/prisma.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [AuthModule, prismaModule],
+  imports: [AuthModule, prismaModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
